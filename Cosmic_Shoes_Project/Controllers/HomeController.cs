b4924006f0f5918c50d248bd45d680d97875bf95
Daniel Barros <daniel.barros@cqumail.com>
2023-08-17 23:09:11 +0800

    Initial commit.

---
+++ Cosmic_Shoes_Project/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cosmic_Shoes_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
