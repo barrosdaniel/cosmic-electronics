d33c4254c4448b36f119add94b5e8873d87c6697
Daniel Barros <daniel.barros@cqumail.com>
2023-08-20 07:57:05 +0800

    Feature: Add Catalogue View.

---
+++ Cosmic_Shoes_Project/Controllers/CatalogueController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cosmic_Shoes_Project.Controllers
+{
+    public class CatalogueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
