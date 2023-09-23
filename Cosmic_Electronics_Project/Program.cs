6aa33c3f23a19ccb05b887540c7bfd28858e714c
Daniel Barros <daniel.barros@cqumail.com>
2023-09-23 12:00:44 +0800

    Configure Entity Framework Core

--- Cosmic_Electronics_Project/Program.cs
+++ Cosmic_Electronics_Project/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Cosmic_Electronics_Project.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StoreDbContext>(opts => {
+    opts.UseSqlServer(
+        builder.Configuration["ConnectionStrings:CosmicElectronicsConnection"]);
+});
 
 var app = builder.Build();
 
