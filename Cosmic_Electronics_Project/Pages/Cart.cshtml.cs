1924889c13f37b4a00ba03f8d87517de0c422724
Daniel Barros <daniel.barros@cqumail.com>
2023-09-23 19:04:40 +0800

    Creating a Razor Page

---
+++ Cosmic_Electronics_Project/Pages/Cart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cosmic_Electronics_Project.Pages
+{
+    public class CartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
