b1281d630aecab561262b5ff2df860c807bd155f
Daniel Barros <daniel.barros@cqumail.com>
2023-09-23 21:28:17 +0800

    Completing the Razor Page

--- Cosmic_Electronics_Project/Pages/Cart.cshtml.cs
+++ Cosmic_Electronics_Project/Pages/Cart.cshtml.cs
@@ -1,18 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Cosmic_Electronics_Project.Infrastructure;
+using Cosmic_Electronics_Project.Models;
 
 namespace Cosmic_Electronics_Project.Pages
 {
     public class CartModel : PageModel
     {
-        [BindProperty]
-        public int ProductID { get; set; }
+        private IStoreRepository repository;
+
+        public CartModel(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        public Cart? Cart { get; set; }
+        public string ReturnUrl { get; set; } = "/";
 
         [BindProperty(Name = "quantity")]
         public int Quantity { get; set; }
 
-        public void OnGet()
+        public void OnGet(string returnUrl)
         {
+            ReturnUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+
+        public IActionResult OnPost(long productId, string returnUrl)
+        {
+            Product? product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+                Cart.AddItem(product, Quantity);
+                HttpContext.Session.SetJson("cart", Cart);
+            }
+            return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
 }
