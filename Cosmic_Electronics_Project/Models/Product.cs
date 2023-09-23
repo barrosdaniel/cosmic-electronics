57a7364e2d21d373319bcc1d687cc01c5ebe7407
Daniel Barros <daniel.barros@cqumail.com>
2023-09-23 10:58:06 +0800

    Install Database Packages

--- Cosmic_Electronics_Project/Models/Product.cs
+++ Cosmic_Electronics_Project/Models/Product.cs
@@ -5,12 +5,12 @@ namespace Cosmic_Electronics_Project.Models
 	public class Product
 	{
 		public int ProductID { get; set; }
-		public string Name { get; set; }
-		public string Description { get; set; }
-		public string Category { set; get; }
+		public string? Name { get; set; }
+		public string? Description { get; set; }
+		public string? Category { set; get; }
 
 		[Column(TypeName = "decimal(8, 2)")]
 		public decimal Price { get; set; }
-		public string Image { set; get; }
+		public string? Image { set; get; }
 	}
 }
