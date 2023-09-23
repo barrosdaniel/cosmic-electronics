053084694fbf1fb9ef783d8e6eba8cfac7d55603
Daniel Barros <daniel.barros@cqumail.com>
2023-09-23 13:30:54 +0800

    Catalogue Product Filter

---
+++ Cosmic_Electronics_Project/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cosmic_Electronics_Project.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "Hello from the Nav View Component";
+        }
+    }
+}
